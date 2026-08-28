966c4598cef33a0249b2aa6ce78706e6e26caa5a
Ran Cheng <752050943@qq.com>
2026-08-28 21:32:23 +0800

    Add C-ary coding starter API

---
+++ starter/coding.py
@@ -0,0 +1,9 @@
+"""Candidate starter for the coding track."""
+
+
+def optimal_prefix_code(probabilities, alphabet_size):
+    raise NotImplementedError("Implement a C-ary prefix-code constructor")
+
+
+def causal_code(class_id, state_probability, alphabet_size):
+    raise NotImplementedError("Aggregate class probabilities and construct a C-ary prefix code")
